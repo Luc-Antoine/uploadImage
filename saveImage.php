1d1853324570068920d9a43b907e2888eba8e53a
Luc-Antoine <lucantoine.dupont@gmail.com>
2020-05-25 22:36:29 +0200

    Update saveImage.php

--- saveImage.php
+++ saveImage.php
@@ -1,28 +1,34 @@
-$firstName = $_POST["firstName"];
-$lastName = $_POST["lastName"];
-$userId = $_POST["userId"];
+<?php
 
-$target_dir = "media";
+class SaveImage {
 
-if(!file_exists($target_dir)) {
-    mkdir($target_dir, 0777, true);
+    public $response;
+
+    public function __construct() {
+        $result=$this->save();
+        $response['error']=$result;
+        $json = json_encode($response);
+        echo $json;
+    }
+    
+    private function save() {
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $userId = $_POST["userId"];
+    
+        $target_dir = "images";
+        if(!file_exists($target_dir)) {
+            mkdir($target_dir, 0777, true);
+        }
+    
+        $target_dir = $target_dir . "/" . $_FILES["file"]["name"];
+    
+        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir)) {
+            return "Good";
+        } else {
+            return 'Upload image failed';
+        }
+    }
 }
 
-$target_dir = $target_dir . "/" . basename($_FILES["file"]["name"]);
-
-if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir)) {
-    echo json_encode([
-        "Message" => "The file ". basename( $_FILES["file"]["name"]). " has been uploaded.",
-        "Status" => "OK",
-        "userId" => $_REQUEST["userId"]
-    ]);
-
-} else {
-
-    echo json_encode([
-        "Message" => "Sorry, there was an error uploading your file.",
-        "Status" => "Error",
-        "userId" => $_REQUEST["userId"]
-    ]);
-
-}
+$saveImage = new SaveImage();
